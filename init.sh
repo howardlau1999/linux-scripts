4423e657f3923f754568cc6ef2618f58215e36e9
Howard Lau <howardlau1999@hotmail.com>
2018-11-23 23:58:06 +0800

    Node.js

--- init.sh
+++ init.sh
@@ -7,9 +7,13 @@ sh ./Anaconda3.sh -b -p $HOME/anaconda3
 echo "Setting up ~/.bashrc..."
 export PATH="$HOME/anaconda3/bin:$PATH"
 echo "PATH=\$HOME/anaconda3/bin:\$PATH" >> ~/.bashrc
+echo "Installing Node.js"
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+source ~/.bashrc
 echo "Setting up mirrors..."
 wget https://tuna.moe/oh-my-tuna/oh-my-tuna.py
 python oh-my-tuna.py
+npm install -g cnpm --registry=https://registry.npm.taobao.org
 pip install -U pip
 conda config --add channels https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud/pytorch/
 conda config --add channels https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud/conda-forge/
@@ -23,5 +27,3 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/mas
 echo "Deep learning things..."
 conda install -y pytorch-nightly-cpu pytorch 
 pip install -U tensorflow tensorflow-gpu
-
-
